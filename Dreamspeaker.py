4394b669078b0e741e394732b88155a13dc5c55b
Stonley890 <79172597+Stonley890@users.noreply.github.com>
2023-12-02 20:29:17 -0800

    First commit

---
+++ Dreamspeaker.py
@@ -0,0 +1,145 @@
+from asyncio import sleep
+import os
+import random
+import discord
+from discord.ext import commands
+from discord.utils import get
+import torch
+from TTS.api import TTS
+from io import BytesIO
+
+# Replace 'YOUR_DISCORD_BOT_TOKEN' with your bot token
+TOKEN = 'YOUR_DISCORD_BOT_TOKEN'
+
+# Discord bot setup
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# List available 🐸TTS models
+print(TTS().list_models())
+
+# Init TTS
+engine = TTS("tts_models/multilingual/multi-dataset/xtts_v1.1").to(device)
+
+SERVING_REQUEST = False
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}\nUse !tts <message> to use TTS!')
+
+@bot.command(pass_context=True)
+async def tts(ctx, *, text):
+    
+    global SERVING_REQUEST
+        
+    # Make sure message isn't empty
+    if text is None:
+        print("Message is empty.")
+        await ctx.send("Your message cannot be empty.")
+        return
+    
+    # Replace acronym with word
+    fixedtext = " " + str(text) + " "
+    fixedtext = fixedtext.replace(" brb ","be right back").replace(" fr ","for real").replace(" imo ","in my opinion").replace(" tbh ","to be honest").replace(" wb ","welcome back").replace(" rn ","right now").replace(" nvm ","never mind").replace(" idk ","I don't know").replace(" ig ","I guess").replace(" dw ","don't worry").strip()
+    
+    # Gets voice channel of message author
+    try:
+        voice_channel = ctx.author.voice.channel
+    except AttributeError:
+        print("Not in a voice channel.")
+        await ctx.send("You are not in a voice channel.")
+        return
+    
+    # Get reference file from username
+    username = ctx.message.author.name
+    print("Getting reference for " + username)
+    file = "reference/" + username + ".wav"
+    
+    # Check if exists
+    if not os.path.isfile(file):
+        print("Reference file does not exist.")
+        await ctx.send("You do not have a reference voice clip.")
+        return
+
+    # Create random filename (in case of multiple requests)
+    filename = "generated/" + str(random.randint(0, 1000)) + ".wav"
+    
+    # Run TTS
+    # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
+    # Text to speech to a file
+    engine.tts_to_file(text=fixedtext, speaker_wav=file, language="en", file_path=filename)
+    print("File written.")
+
+    # Send the synthesized voice as a Discord voice message
+    # await ctx.send("Recreating voice...", file=discord.File('output.wav'))
+    
+    # Wait for previous request to complete
+    while SERVING_REQUEST:
+        await sleep(.1)
+    print("Serving file.")
+    
+    SERVING_REQUEST = True
+    
+    # Play the message
+    try:
+        vc = await voice_channel.connect()
+    except discord.errors.ClientException:
+        vc = get(bot.voice_clients, guild=ctx.guild)
+        print("Already connected to VC.")
+    vc.play(discord.FFmpegPCMAudio(executable="ffmpeg", source=filename))
+    # Sleep while audio is playing.
+    while vc.is_playing():
+        await sleep(.1)
+    # await vc.disconnect()
+    
+    # Delete file after playing
+    os.remove(filename)
+    
+    SERVING_REQUEST = False
+    print("Done!")
+
+@bot.command(pass_context=True)
+async def voice(ctx, voice, *, text):
+            
+    # Make sure message isn't empty
+    if text is None:
+        print("Message is empty.")
+        await ctx.send("Your message cannot be empty.")
+        return
+
+    # Replace acronym with word
+    fixedtext = " " + str(text) + " "
+    fixedtext = fixedtext.replace(" brb ","be right back").replace(" fr ","for real").replace(" imo ","in my opinion").replace(" tbh ","to be honest").replace(" wb ","welcome back").replace(" rn ","right now").replace(" nvm ","never mind").replace(" idk ","I don't know").replace(" ig ","I guess").replace(" dw ","don't worry").strip()
+    
+    # Get reference file from username
+    print("Getting reference for " + voice)
+    file = "voices/" + voice + ".wav"
+    
+    # Check if exists
+    if not os.path.isfile(file):
+        print("Reference file does not exist.")
+        await ctx.send("That voice could not be found.")
+        return
+
+    # Create random filename (in case of multiple requests)
+    filename = "generated/" + str(random.randint(0, 1000)) + ".wav"
+    
+    # Run TTS
+    # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
+    # Text to speech to a file
+    engine.tts_to_file(text=fixedtext, speaker_wav=file, language="en", file_path=filename)
+    print("File written.")
+
+    # Send the synthesized voice as a Discord voice message
+    await ctx.send("File generated.", file=discord.File(filename))
+    
+    # Delete file
+    os.remove(filename)
+    
+    print("Done!")
+
+bot.run(TOKEN)
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Dreamspeaker
- A deep learning Discord TTS bot that clones voices.
+A deep learning Discord TTS bot that clones voices.
+
+This project actually started off as a joke. Me and my friends like to use a TTS bot so that if someone can’t speak, they can still take part in conversation. I find that it’s easy for people to ignore mute users because you have to actively monitor the chat. When using a TTS bot, people can better participate in conversation because you can actually be heard.
+
+The only problem is that it becomes more difficult to figure out who is speaking, especially when multiple people are using TTS. Hearing a distinct and recognizable voice allows you to subconsciously link the voice to a person at any time in the sentence.
+
+All of the actual AI processing is from a project called [coqui](https://github.com/coqui-ai/TTS). It’s a super capable toolkit and all credit for the voice processing goes to them. To actually make it clone a voice, it needs a reference recording. Any recording will do, but I’ve found it works best when reading the FitnessGram Pacer Test introduction. Then, test with some different phrases and if the bot struggles with a sentence, add to the recording your own reading of the sentence. Rinse and repeat until it can properly speak sentences. The bot automatically uses the correct reference based on the command sender. That way, only you can use your voice.
+
+## Installation
+
+This installation is intended for Ubuntu 18.04 / 20.04 / 22.04. I hope to add Windows 10/11 and MacOS automatic installation in the future.
+
+### 1. Create a Discord bot
+1. Go to https://discord.com/developers/applications and create a new application. Give it any name, description, or icon.
+2. Go to the **Bot** tab and create a bot. Give it a name.
+3. Scroll down and look for MESSAGE CONTENT INTENT under _Privileged Gateway Intents_. Enable the toggle.
+4. Under the **OAuth2** tab, go to **URL Generator**. Check the _bot_ box. In the second table, check the _Read Messages/View Channels_, _Connect_, _Speak_, _Send Messages_, and _Attach Files_ boxes.
+5. Copy the URL and open it. Invite the bot to your server. Go back to the **Bot** tab and find the _Bot Token_. Reset and copy it. We'll need it later.
+
+### 2. Install
+Run the  `install.sh` script. If double-clicking it does not work, enable execution in the properties or use `bash path/to/install.sh` in a terminal.
+
+Paste the bot token into the prompt, then wait while installation occurs. After installation, you will be asked if you want to run the bot. Whether you do or not, you will need to accept coqui's EULA and download the XTTS v1.1 model when you run the bot for the first time. Simple type `y` and press enter at the prompts.
+
+### 3. Setup
+You're done with installation! Now you just need to add reference audio files. See below for more information.
+
+## Commands
+
+### `!tts <message>`
+
+Use TTS. This command must be used while in a VC. The user must have their own reference audio file in the `reference` folder with the name `[usermame].wav`, replacing `[username]` with the username of the user (not their server nickname).
+
+### `!voice <voice> <message>`
+
+Generate a file from a voice. Voices are stored in the `voices` folder. I've included the `dramatic` voice, which comes from a public-domain LibreVox audiobook.

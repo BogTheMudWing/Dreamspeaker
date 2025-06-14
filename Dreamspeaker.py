f0a4bd28555f7750f8701bc60191b40c1a8f141d
Bog <stonley@macver.org>
2025-06-13 21:24:28 -0700

    Add quality of life and performance optimizations

--- Dreamspeaker.py
+++ Dreamspeaker.py
@@ -1,15 +1,22 @@
 from asyncio import sleep
+import asyncio
 import os
 import random
 import discord
 from discord.ext import commands
 from discord.utils import get
 import torch
+import torchaudio
+from pydub import AudioSegment, effects
+import time
+import typing
+import functools
+import emoji
+from collections import deque
 from TTS.api import TTS
-from io import BytesIO
 
-# Replace 'YOUR_DISCORD_BOT_TOKEN' with your bot token
-TOKEN = 'YOUR_DISCORD_BOT_TOKEN'
+# Replace '127' with your bot token
+TOKEN = 'YOUR_BOT_TOKEN'
 
 # Discord bot setup
 intents = discord.Intents.default()
@@ -19,37 +26,73 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 # Get device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# List available 🐸TTS models
-print(TTS().list_models())
-
 # Init TTS
-engine = TTS("tts_models/multilingual/multi-dataset/xtts_v1.1").to(device)
+engine = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
 # Ensures only one clip plays at a time
 SERVING_REQUEST = False
 
 # Ensures clips are played in order of request
-queuedClips = list()
+queuedClips: deque = deque()
+requests: deque = deque()
+
+# engine.tts_to_file(text=fixedtext, speaker_wav=file, language="en", file_path=filename)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}\nUse !tts <message> to use TTS!')
+    print(f"Logged in as {bot.user.name}\nUse !tts <message> to use TTS!")
+
+
+def to_thread(func: typing.Callable) -> typing.Coroutine:
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        return await asyncio.to_thread(func, *args, **kwargs)
+
+    return wrapper
+
+
+@to_thread
+def generate(file, fixedtext, filename):
+    engine.tts_to_file(text=fixedtext, speaker_wav=file, language="en", file_path=filename)
+    return
+
 
 @bot.command(pass_context=True)
 async def tts(ctx, *, text):
-    
-    global SERVING_REQUEST
-        
+
+    global queuedClips
+    global requests
+
     # Make sure message isn't empty
     if text is None:
         print("Message is empty.")
         await ctx.send("Your message cannot be empty.")
         return
-    
+
     # Replace acronym with word
     fixedtext = " " + str(text) + " "
-    fixedtext = fixedtext.replace(" brb ","be right back").replace(" fr ","for real").replace(" imo ","in my opinion").replace(" tbh ","to be honest").replace(" wb ","welcome back").replace(" rn ","right now").replace(" nvm ","never mind").replace(" idk ","I don't know").replace(" ig ","I guess").replace(" dw ","don't worry").strip()
-    
+    fixedtext = (
+        fixedtext.replace(" brb ", " be right back ")
+        .replace(" fr ", " for real ")
+        .replace(" imo ", " in my opinion ")
+        .replace(" tbh ", " to be honest ")
+        .replace(" wb ", " welcome back ")
+        .replace(" rn ", " right now ")
+        .replace(" nvm ", " never mind ")
+        .replace(" idk ", " I don't know ")
+        .replace(" ig ", " I guess ")
+        .replace(" dw ", " don't worry ")
+        .replace(" yk ", " you know ")
+        .replace(" thx ", " thanks ")
+        .replace(" gn ", " good night ")
+        .replace(" mb ", " my bad ")
+        .replace(" smth ", " something ")
+        .replace(" fsr ", " for some reason ")
+        .strip()
+    )
+    # Replace emojis with their names
+    fixedtext = emoji.demojize(fixedtext)
+
     # Gets voice channel of message author
     try:
         voice_channel = ctx.author.voice.channel
@@ -57,12 +100,12 @@ async def tts(ctx, *, text):
         print("Not in a voice channel.")
         await ctx.send("You are not in a voice channel.")
         return
-    
+
     # Get reference file from username
     username = ctx.message.author.name
     print("Getting reference for " + username)
     file = "reference/" + username + ".wav"
-    
+
     # Check if exists
     if not os.path.isfile(file):
         print("Reference file does not exist.")
@@ -71,91 +114,95 @@ async def tts(ctx, *, text):
 
     # Create random filename (in case of multiple requests)
     filename = "generated/" + str(random.randint(0, 1000)) + ".wav"
-    
-    # Run TTS
-    # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
-    # Text to speech to a file
-    engine.tts_to_file(text=fixedtext, speaker_wav=file, language="en", file_path=filename)
-    print("File written.")
+    requests.append(filename)
+    print("Added to queue")
 
     # Add to list
     queuedClips.append(filename)
-    
+
+    asyncio.create_task(ctx.message.add_reaction("⏳"))
+
     # Wait for previous request to complete
-    while SERVING_REQUEST:
-        await sleep(.1)
+    while requests[0] is not filename:
+        await sleep(0.1)
+    print("Generating file.")
+
+    # Run TTS
+    # Text to speech to a file
+    asyncio.create_task(ctx.message.clear_reaction("⏳"))
+    asyncio.create_task(ctx.message.add_reaction("⚙️"))
+
+    await generate(file, fixedtext, filename)
+
+    requests.popleft()
+
+    # Normalize audio
+    rawsound = AudioSegment.from_file(filename, "wav")
+    normalizedsound = effects.normalize(rawsound)
+    normalizedsound.export(filename, format="wav")
+
+    print("File written.")
+
+    asyncio.create_task(ctx.message.clear_reaction("⚙️"))
+    asyncio.create_task(ctx.message.add_reaction("🔈"))
+
+    # Wait for previous request to complete
+    while queuedClips[0] is not filename:
+        await sleep(0.1)
     print("Serving file.")
-    
-    SERVING_REQUEST = True
-    
+
     # Play the message
     try:
+        print("Attempting to connnect to VC.")
         vc = await voice_channel.connect()
+        print("Connected.")
+        await sleep(3)
     except discord.errors.ClientException:
         vc = get(bot.voice_clients, guild=ctx.guild)
         print("Already connected to VC.")
-    vc.play(discord.FFmpegPCMAudio(executable="ffmpeg", source=queuedClips[0]))
+
+    print("Connected to voice. Playing file...")
+    try:
+        asyncio.create_task(ctx.message.clear_reaction("🔈"))
+        asyncio.create_task(ctx.message.add_reaction("🔊"))
+        vc.play(discord.FFmpegPCMAudio(executable="ffmpeg", source=queuedClips[0]))
+    except discord.ClientException:
+        print("Client error. Aborting.")
+        return
+    print("Playback started")
+
     # Sleep while audio is playing.
     while vc.is_playing():
-        await sleep(.1)
-    # await vc.disconnect()
-    
+        await sleep(0.1)
+
+    print("Playback finished")
+    asyncio.create_task(ctx.message.clear_reaction("🔊"))
+    asyncio.create_task(ctx.message.add_reaction("✅"))
+
     # Delete file after playing
-    os.remove(queuedClips.pop(0))
-    
-    SERVING_REQUEST = False
+    os.remove(queuedClips.popleft())
+
     print("Done!")
 
-@bot.command(pass_context=True)
-async def voice(ctx, voice, *, text):
-            
-    # Make sure message isn't empty
-    if text is None:
-        print("Message is empty.")
-        await ctx.send("Your message cannot be empty.")
-        return
-
-    # Replace acronym with word
-    fixedtext = " " + str(text) + " "
-    fixedtext = fixedtext.replace(" brb ","be right back").replace(" fr ","for real").replace(" imo ","in my opinion").replace(" tbh ","to be honest").replace(" wb ","welcome back").replace(" rn ","right now").replace(" nvm ","never mind").replace(" idk ","I don't know").replace(" ig ","I guess").replace(" dw ","don't worry").strip()
-    
-    # Get reference file from username
-    print("Getting reference for " + voice)
-    file = "voices/" + voice + ".wav"
-    
-    # Check if exists
-    if not os.path.isfile(file):
-        print("Reference file does not exist.")
-        await ctx.send("That voice could not be found.")
-        return
-
-    # Create random filename (in case of multiple requests)
-    filename = "generated/" + str(random.randint(0, 1000)) + ".wav"
-    
-    # Run TTS
-    # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
-    # Text to speech to a file
-    engine.tts_to_file(text=fixedtext, speaker_wav=file, language="en", file_path=filename)
-    print("File written.")
-
-    # Send the synthesized voice as a Discord voice message
-    await ctx.send("File generated.", file=discord.File(filename))
-    
-    # Delete file
-    os.remove(filename)
-    
-    print("Done!")
 
 @bot.command(pass_context=True)
 async def leave(ctx):
-    
+
     if not ctx.voice_client:
-        await ctx.send('I am not currently connected to a voice channel.')
+        await ctx.send("I am not currently connected to a voice channel.")
         return
-    
-    if ctx.author.voice.channel and ctx.author.voice.channel == ctx.voice_client.channel:
+
+    if (
+        ctx.author.voice.channel
+        and ctx.author.voice.channel == ctx.voice_client.channel
+    ):
         await ctx.voice_client.disconnect()
+        queuedClips.clear()
+        requests.clear()
     else:
-        await ctx.send('You have to be connected to the same voice channel to disconnect me.')
+        await ctx.send(
+            "You have to be connected to the same voice channel to disconnect me."
+        )
+
 
 bot.run(TOKEN)
